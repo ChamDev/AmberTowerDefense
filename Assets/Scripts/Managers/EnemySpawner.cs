d35430f6fc036cb70507690681d0a2bef380f2c8
Antonio Cham <antonio.cham@alumnos.udg.mx>
2022-02-26 19:11:54 -0600

    Added EnemySpawner and centralized GameManager

---
+++ Assets/Scripts/Managers/EnemySpawner.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections;
+using Unity.Mathematics;
+using UnityEngine;
+using Random = UnityEngine.Random;
+
+namespace Managers
+{
+    public class EnemySpawner : MonoBehaviour
+    {
+        
+        [SerializeField] private int enemiesPerWave = 5;
+        [SerializeField] private int increaseEnemiesPerWave = 3;
+        [SerializeField] private int numberOfWaves = 2;
+        [SerializeField] private float timeMaxToWait = 5;
+        [SerializeField] private GameObject[] enemyPrefabs;
+        [SerializeField] private Transform spawnPoint;
+
+        private int _enemyIndex = 0;
+        private GameManager _gameManager;
+        private float _timeToWait = 1;
+        private void Awake()
+        {
+            _gameManager = FindObjectOfType<GameManager>();
+        }
+
+        private void Start()
+        {
+            StartCoroutine(EnemyWaveSpawner());
+        }
+
+        IEnumerator EnemyWaveSpawner()
+        {
+            for (int i = 0; i < numberOfWaves; i++)
+            {
+                yield return SpawningEnemies();
+
+                enemiesPerWave += increaseEnemiesPerWave;
+            }
+            
+            _gameManager.GameOver(true);
+        }
+
+        IEnumerator SpawningEnemies()
+        {
+            _gameManager.NumberEnemiesToDefeat = enemiesPerWave;
+            _enemyIndex = Random.Range(0, enemyPrefabs.Length);
+            
+            for (int i = 0; i < enemiesPerWave; i++)
+            {
+                Instantiate(enemyPrefabs[_enemyIndex], spawnPoint.position, quaternion.identity);
+
+                _timeToWait = Random.Range(0, timeMaxToWait);
+
+                yield return new WaitForSeconds(_timeToWait);
+            }
+            
+            yield return new WaitUntil(() => _gameManager.NumberEnemiesToDefeat <= 0);
+        }
+    }
+}
--- Assets/Scripts/Managers/GameManager.cs
+++ Assets/Scripts/Managers/GameManager.cs
@@ -1,4 +1,7 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
+using Ui;
+using UI;
 using UnityEngine;
 
 namespace Managers
@@ -8,8 +11,31 @@ namespace Managers
 		[SerializeField] private GameObject winningScreen;
 		[SerializeField] private GameObject losingScreen;
 		
+		private int _numberEnemiesToDefeat;
 		private bool _isPointerOnAllowedArea = false;
+		private int _enemyScore = 5;
 		
+		private Score _score;
+		private HealthBar _playerHealth;
+
+		private void Awake()
+		{
+			_score = GameObject.FindObjectOfType<Score>();
+		}
+
+
+		public int NumberEnemiesToDefeat
+		{
+			get => _numberEnemiesToDefeat;
+			set => _numberEnemiesToDefeat = value;
+		}
+
+		public int EnemyScore
+		{
+			get => _enemyScore;
+			set => _enemyScore = value;
+		}
+
 		public bool isPointerOnAllowedArea()
 		{
 			return _isPointerOnAllowedArea;
@@ -20,7 +46,7 @@ namespace Managers
 			_isPointerOnAllowedArea = isPointerAllowed;
 		}
 		
-		private void GameOver(bool playerHasWon)
+		public void GameOver(bool playerHasWon)
 		{
 			if (playerHasWon)
 			{
@@ -32,6 +58,29 @@ namespace Managers
 			}
 			
 			Time.timeScale = 0;
+		}
+
+		public void AddScore(int score)
+		{
+			_score.AddScore(score);
+		}
+
+		public void DamagePlayerHealth(int damage)
+		{
+			var playerDead = _playerHealth.ApplyDamage(damage);
+
+			if (playerDead)
+			{
+				GameOver(false);
+			}
+			
+			EnemyDefeated();
+		}
+		
+		public void EnemyDefeated()
+		{
+			NumberEnemiesToDefeat--;
+			AddScore(_enemyScore);
 		}
 	}
 }
